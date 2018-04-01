baecba591633e7c41b0992376e441c8ffa020b9e
Kevin Wang (GIS) <kevin10410@gmail.com>
2018-04-01 15:33:39 +0800

    first commit

---
+++ src/js/dom-loader.js
@@ -0,0 +1,7 @@
+//export the code will be used
+export var secretButton = document.querySelector('#secret-button');
+export var secretParagraph = document.querySelector('#secret-paragraph');
+
+
+// var secretButton = document.querySelector('#secret-button');
+// var secretParagraph = document.querySelector('#secret-paragraph');
